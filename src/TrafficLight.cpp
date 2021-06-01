d0ca33cf18e3e9df576e65da106a92138599d99a
Alexandru Noaja <noaja.alexandru@gmail.com>
2021-06-01 13:23:35 +0000

    Initial commit and implementation of Udacity Project tasks
    Signed-off-by: Alexandru Noaja <noaja.alexandru@gmail.com>

---
+++ src/TrafficLight.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <random>
+#include <future>
+#include "TrafficLight.h"
+
+std::random_device rd;
+std::mt19937 eng(rd());
+std::uniform_int_distribution<int> distr(4000, 6000);
+
+/* Implementation of class "MessageQueue" */
+template <typename T>
+T MessageQueue<T>::receive()
+{
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> ulock(_queueMutex);
+    _cond.wait(ulock);
+  
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+    return msg;
+}
+
+template <typename T>
+void MessageQueue<T>::send(T &&msg)
+{
+    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lockGuard(_queueMutex);
+
+    _queue.clear();
+    _queue.push_back(std::move(msg));
+
+    _cond.notify_one();
+}
+
+
+/* Implementation of class "TrafficLight" */
+
+ 
+TrafficLight::TrafficLight()
+{
+    _currentPhase = TrafficLightPhase::red;
+}
+
+
+void TrafficLight::waitForGreen()
+{
+    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // runs and repeatedly calls the receive function on the message queue. 
+    // Once it receives TrafficLightPhase::green, the method returns.
+    while (true)
+    {
+      auto msg = _msg_queue.receive();
+      
+      if (msg == TrafficLightPhase::green)
+        break;
+    }
+  
+    return;
+      
+}
+
+TrafficLightPhase TrafficLight::getCurrentPhase()
+{
+    return _currentPhase;
+}
+
+void TrafficLight::simulate()
+{
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+  threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
+}
+
+// virtual function which is executed in a thread
+void TrafficLight::cycleThroughPhases()
+{
+    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
+    // and toggles the current phase of the traffic light between red and green and sends an update method 
+    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+ 
+  double cycle_duration = distr(eng);
+  std::cout << "Traffic Light " << _id << "started" << cycle_duration << "\n";
+  std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+  lastUpdate = std::chrono::system_clock::now();
+  
+  while (true)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    long timeSinceUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-lastUpdate).count();
+    
+    if (timeSinceUpdate >= cycle_duration)
+    {
+      std::unique_lock<std::mutex> uLock(_mutex);
+      // uLock.lock();
+      if (_currentPhase == TrafficLightPhase::red)
+      {
+        _currentPhase = TrafficLightPhase::green;
+        std::cout << "Traffic Light " << _id << "Green\n";
+      }
+      else
+      {
+        _currentPhase = TrafficLightPhase::red;
+        std::cout << "Traffic Light " << _id << "Red\n";
+      }
+
+      uLock.unlock();
+      
+      // update to message queue
+      _msg_queue.send(std::move(_currentPhase));
+      
+      // reset stop watch
+      lastUpdate = std::chrono::system_clock::now();
+      
+      // choose randomly cycle duration for next cycle
+      cycle_duration = distr(eng);
+        
+    }
+      
+          
+  }
+    
+}
